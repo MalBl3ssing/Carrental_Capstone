924784b9ddfbeac82d77b882b40cf5303a25e5d0
ThandekaChantal <malandethandeka@gmail.com>
2026-06-21 16:27:22 +0200

    Add Insurance entity with Builder Pattern - closes #ISSUE-NUMBER

--- src/main/java/za/ac/cput/carrental/domain/Insurance.java
+++ src/main/java/za/ac/cput/carrental/domain/Insurance.java
@@ -1,4 +1,92 @@
+/* Insurance.java
+   Insurance entity class - Builder Pattern
+   Author: Thandeka Chantal Malande
+   Date: 21 June 2026 */
+
 package za.ac.cput.carrental.domain;
 
 public class Insurance {
+
+
+    private String insuranceId;
+    private String bookingId;
+    private String type;
+    private double dailyPremium;
+
+
+    private Insurance(Builder builder) {
+        this.insuranceId  = builder.insuranceId;
+        this.bookingId    = builder.bookingId;
+        this.type         = builder.type;
+        this.dailyPremium = builder.dailyPremium;
+    }
+
+
+    public String getInsuranceId() {
+        return insuranceId;
+    }
+
+    public String getBookingId() {
+        return bookingId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getDailyPremium() {
+        return dailyPremium;
+    }
+
+
+    public double calculatePremiumTotal(int days) {
+        return dailyPremium * days;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Insurance{" +
+                "insuranceId='" + insuranceId + '\'' +
+                ", bookingId='" + bookingId + '\'' +
+                ", type='" + type + '\'' +
+                ", dailyPremium=" + dailyPremium +
+                '}';
+    }
+
+
+    public static class Builder {
+
+
+        private String insuranceId;
+        private String bookingId;
+        private String type;
+        private double dailyPremium;
+
+
+        public Builder setInsuranceId(String insuranceId) {
+            this.insuranceId = insuranceId;
+            return this;
+        }
+
+        public Builder setBookingId(String bookingId) {
+            this.bookingId = bookingId;
+            return this;
+        }
+
+        public Builder setType(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public Builder setDailyPremium(double dailyPremium) {
+            this.dailyPremium = dailyPremium;
+            return this;
+        }
+
+
+        public Insurance build() {
+            return new Insurance(this);
+        }
+    }
 }
